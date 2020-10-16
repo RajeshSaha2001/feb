97da5b5fe662f20ddaa841527778d4212b1a28fc
Rajesh Chandra Saha <55900566+RajeshSaha2001@users.noreply.github.com>
2020-10-16 18:38:31 +0530

    Add files via upload

---
+++ oct.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class a_length
+{
+
+    /* int length()
+    Returns the length of the string */
+    
+    public st
+        System.out.println("Length="+l);
+    }
+}
