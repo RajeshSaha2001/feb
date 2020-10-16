cf30cd48062c3abb69fe8c2d4d73f354e340f7b5
deep629 <59963147+deep629@users.noreply.github.com>
2020-10-16 18:40:26 +0530

    solved issues

--- oct.java
+++ oct.java
@@ -1,12 +1,39 @@
 import java.util.Scanner;
 
-public class a_length
+public class d_compareToIgnoreCase
 {
 
-    /* int length()
-    Returns the length of the string */
+    /* int compareToIgnoreCase(String str)
+    Compares two strings lexicographically, ignoring case differences.
     
-    public st
-        System.out.println("Length="+l);
+    equalsIgnoreCase(String anotherString)
+    Compares this String to another String, ignoring case considerations. */
+    
+    public static void main(String args[])
+    {
+        String s1,s2;
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("enter 2 strings");
+        s1=sc.nextLine();
+        s2=sc.nextLine();
+        
+        if(s1.compareToIgnoreCase(s2)==0)
+        {
+            System.out.println("Same");
+        }
+        else
+        {
+            System.out.println("Not Same");
+        }
+        
+        if(s1.equalsIgnoreCase(s2)==true)
+        {
+            System.out.println("Same");
+        }
+        else
+        {
+            System.out.println("Not Same");
+        }
     }
 }
